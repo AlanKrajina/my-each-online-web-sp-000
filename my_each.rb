5be682b7e5640e49c34d79fa8007b90228e98d75
Alan Krajina <alankrajina88@gmail.com>
2019-05-11 11:52:43 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,13 @@
-def my_each # put argument(s) here
-  # code here
+
+
+def my_each (collection)
+  count=0
+  while count < collection.length
+  yield
+end
+return collection
+end
+
+my_each([1, 2, 3, 4]) do |i|
+  puts i
 end
